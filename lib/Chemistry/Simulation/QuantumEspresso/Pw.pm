d9223252ca734a8983e13ca82c71b668398113c1
Henning Glawe <glaweh@debian.org>
2010-11-12 15:30:26 +0100

    bump minor

--- lib/Chemistry/Simulation/QuantumEspresso/Pw.pm
+++ lib/Chemistry/Simulation/QuantumEspresso/Pw.pm
@@ -12,7 +12,7 @@ BEGIN {
 	@EXPORT=qw(&parse_pw_out);
 }
 my $parser_version_major=1;
-my $parser_version_minor=3;
+my $parser_version_minor=4;
 
 sub annotate_debug($$$$) {
 	my ($fh,$sub,$parsed,$data)=@_;
