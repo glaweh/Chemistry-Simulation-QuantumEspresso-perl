743f290b73215bffc64c53faafb1da0778ed9d4f
Henning Glawe <glaweh@debian.org>
2011-11-01 09:50:14 +0100

    constructor/init prototype

--- lib/Fortran/Namelist/Editor.pm
+++ lib/Fortran/Namelist/Editor.pm
@@ -3,4 +3,36 @@ use strict;
 use warnings;
 use carp;
 
+sub new {
+	my $class=shift;
+	my %opts=%{pop()} if ($_[-1] and (ref $_[-1] eq 'hash');
+	my $self={
+		filename    => undef,
+		data        => undef,
+		_comments   => [],
+		_strings    => [],
+		_groups     => [],
+		_groupless  => [],
+		_vars       => [],
+		%opts,
+	};
+	bless($self,$class);
+	return(undef) unless $self->init();
+	return($self);
+}
+
+sub init {
+	my $self=shift;
+	if (defined $self->{filename} and (! defined $self->{data})) {
+		# slurp in file
+		if open(my $fh,$self->{filename}) {
+			local $/=undef;
+			$self->{data}=<$fh>;
+		} else {
+			carp "Error opening file '$self->{filename}'";
+			return(undef);
+		}
+	}
+}
+
 1;
