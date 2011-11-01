cfc0f2293c6b86335ae6f2a8de3545a7c03ced99
Henning Glawe <glaweh@debian.org>
2011-11-01 09:50:46 +0100

    comment/string detection

--- lib/Fortran/Namelist/Editor.pm
+++ lib/Fortran/Namelist/Editor.pm
@@ -33,6 +33,51 @@ sub init {
 			return(undef);
 		}
 	}
+	if ($self->{data}) {
+		$self->{data_cs}=$self->find_comments_and_strings();
+	}
+}
+
+sub find_comments_and_strings {
+	my $self = shift;
+	# find all fortran !-style comments and quoted strings
+	while ($data =~ m{
+			                   ## f90 comments start with !, end with EOL
+			(![^\n]*)\n        ## put into group1
+		|                      ## OR ! which are part of quoted strings
+			(                  ##group2
+				'(?:''|[^'])*' ## single quoted strings, '' is an escaped '
+			|
+				"(?:""|[^"])*" ## double quoted strings, "" is an escaped "
+			)                  ##group2-end
+		}xsg) {
+		if (defined $2) {
+			my %string=(
+				o_b => $-[2];
+				o_e => $+[2];
+			);
+			push @{$self->{_strings}},\%string;
+		} else {
+			my %comment=(
+				o_b => $-[1];
+				o_e => $+[1];
+			);
+			push @{$self->{_comments}},\%comment;
+		}
+	}
+	# working copy
+	my $d = $self->{data};
+	# replace comments by same-length sequence of space
+	foreach my $c (@{$self->{_comments}}) {
+		my $len=$c->{o_e}-$c->{o_b};
+		substr($d,$c->o_b,$len) = ' ' x $len;
+	}
+	# replace strings by same-length sequence of underscores
+	foreach my $s (@{$self->{_strings}}) {
+		my $len=$s->{o_e}-$s->{o_b};
+		substr($d,$s->{o_b},$len) = '_' x $len;
+	}
+	return($d);
 }
 
 1;
