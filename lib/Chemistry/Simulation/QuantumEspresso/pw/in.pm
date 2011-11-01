d13b4a9e13de2586bf0a30be040b436e1d129996
Henning Glawe <glaweh@debian.org>
2011-11-01 10:11:16 +0100

    make pw::in inherit from Fortran::Namelist::Editor

--- lib/Chemistry/Simulation/QuantumEspresso/pw/in.pm
+++ lib/Chemistry/Simulation/QuantumEspresso/pw/in.pm
@@ -2,5 +2,7 @@ package Chemistry::Simulation::QuantumEspresso::pw::in;
 use strict;
 use warnings;
 use Carp;
+use Fortran::Namelist::Editor;
+@Chemistry::Simulation::QuantumEspresso::pw::in::ISA=('Fortran::Namelist::Editor');
 
 1;
