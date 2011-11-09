6fa9e1fc1def6bc542eacb22bf82c57456a602b2
Henning Glawe <glaweh@debian.org>
2011-11-09 14:34:10 +0100

    add cell_parameters card parser

--- lib/Chemistry/Simulation/QuantumEspresso/pw/in.pm
+++ lib/Chemistry/Simulation/QuantumEspresso/pw/in.pm
@@ -37,6 +37,9 @@ sub parse_cards {
 			($i,$card)=Chemistry::Simulation::QuantumEspresso::pw::in::card::atomic_positions->new($self,\@lines,\@lines_cs,\@o_lines,$i);
 			next;
 		}
+		if ($lines_cs[$i] =~ /^\s*cell_parameters/i) {
+			($i,$card)=Chemistry::Simulation::QuantumEspresso::pw::in::card::cell_parameters->new($self,\@lines,\@lines_cs,\@o_lines,$i);
+			next;
 		}
 	} continue {
 		if (defined $card) {
@@ -177,6 +180,67 @@ sub parse {
 	$self->{o_e}=$o_lines->[$i];
 	if ($#{$self->{_atom}} < $nat) {
 		warn "Card 'atomic_positions' incomplete";
+	}
+	return($i,$self);
+}
+
+package Chemistry::Simulation::QuantumEspresso::pw::in::card::cell_parameters;
+use strict;
+use warnings;
+@Chemistry::Simulation::QuantumEspresso::pw::in::card::cell_parameters::ISA = qw{Chemistry::Simulation::QuantumEspresso::pw::in::card};
+sub init {
+	my ($self,$container,@args)=@_;
+	$self->SUPER::init($container,@args);
+	$self->{name}     ='cell_parameters';
+	$self->{symmetry} = undef;
+	$self->{v}     = [];
+	if ($#args >= 0) {
+		return($self->parse(@args));
+	}
+	return(undef,$self);
+}
+
+sub parse {
+	my ($self,$lines,$lines_cs,$o_lines,$i) = @_;
+	# check for units
+	my $title = $lines_cs->[$i];
+	while ($title =~ /__+/) {
+		substr($title,$-[0],$+[0]-$-[0])=substr($lines->[$i],$-[0],$+[0]-$-[0]);
+	}
+	if ($title =~ /^\s*(cell_parameters)(\s+\S*(cubic|hexagonal)\S*)?/i) {
+		my $o_line = $o_lines->[$i];
+		$self->{name}=Fortran::Namelist::Editor::Token->new($self->{container},$o_line+$-[1],$o_line+$+[1]);
+		if (defined $2) {
+			$self->{symmetry}=Fortran::Namelist::Editor::Token->new($self->{container},$o_line+$-[1],$o_line+$+[1]);
+		} else {
+			substr($lines->[$i],$+[1],0) = ' cubic';
+			substr($lines_cs->[$i],$+[1],0) = ' cubic';
+			foreach (@{$o_lines}) {
+				$_+=6 if ($_ > $o_line);
+			}
+			$self->{symmetry}=Fortran::Namelist::Editor::Token->insert($self->{container},$o_line+$+[1],'cubic');
+		}
+	}
+
+	while (1) {
+		$i++;
+		last if ($#{$self->{v}} >= 2);
+		last if ($i > $#{$lines});
+		next if ($lines_cs->[$i] =~ /^\s*$/);
+		my $o_line = $o_lines->[$i];
+		if ($lines_cs->[$i] =~ /^\s*(\S+)\s+(\S+)\s+(\S+)/) {
+			warn 'da';
+			my $vec = [
+				Fortran::Namelist::Editor::Value::single->new($self->{container},$o_line+$-[1],$o_line+$+[1]),
+				Fortran::Namelist::Editor::Value::single->new($self->{container},$o_line+$-[2],$o_line+$+[2]),
+				Fortran::Namelist::Editor::Value::single->new($self->{container},$o_line+$-[3],$o_line+$+[3]),
+			];
+			push @{$self->{v}},$vec;
+		}
+	}
+	$self->{o_e}=$o_lines->[$i];
+	if ($#{$self->{v}} < 2) {
+		warn "Card 'cell_parameters' incomplete";
 	}
 	return($i,$self);
 }
