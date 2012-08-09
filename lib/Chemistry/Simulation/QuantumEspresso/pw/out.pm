9e72b11890aab7a353daeae4d88de5035da2fc40
Henning Glawe <glaweh@debian.org>
2012-08-09 13:38:33 +0200

    rename module Pw to pw::out

--- lib/Chemistry/Simulation/QuantumEspresso/pw/out.pm
+++ lib/Chemistry/Simulation/QuantumEspresso/pw/out.pm
@@ -1,4 +1,4 @@
-package Chemistry::Simulation::QuantumEspresso::Pw;
+package Chemistry::Simulation::QuantumEspresso::pw::out;
 # Parser for Quantum Espresso's pw.x output
 # Copyright (C) 2006-2010 Henning Glawe <glaweh@debian.org>
 #
