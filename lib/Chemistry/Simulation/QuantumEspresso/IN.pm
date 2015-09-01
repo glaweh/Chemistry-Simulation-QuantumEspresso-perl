4409d94b0966e5c91e983604d7a450d834df990d
Henning Glawe <glaweh@debian.org>
2015-09-01 20:35:35 +0200

    fallback to Fortran::Namelist::Editor for espresso input

---
+++ lib/Chemistry/Simulation/QuantumEspresso/IN.pm
@@ -0,0 +1,6 @@
+package Chemistry::Simulation::QuantumEspresso::IN;
+use strict;
+use warnings;
+use Fortran::Namelist::Editor;
+@Chemistry::Simulation::QuantumEspresso::IN::ISA=('Fortran::Namelist::Editor');
+1;
